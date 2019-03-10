4bff5d5e80bbff4bb4e007ae3ad00151171ea49d
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-03-10 15:53:20 -0600

    Update HeroImage.js

--- HeroImage.js
+++ HeroImage.js
@@ -3,10 +3,25 @@ import './HeroImage.css';
 
 const HeroImage = (props) => {
     return(
-        <div>
-            HeroImage
+        <div className="rndb-heroimage"
+            style = {{
+                background:
+                `linear-gradient(to bottom, rgba(0, 0, 0, 0)
+                39%, rgba(0, 0, 0, 0)
+                41%, rgba(0, 0, 0, 0.65)
+                100%),
+                url('${props.image}'), #1c1c1c`//use backtics for this expression to take a props
+            }}
+        >
+            <div className="rmdb-heroimage-content">
+                <div className="rmdb-heroimage-text">
+                <h1>{ props.title }</h1>
+                <p>{ props.text }</p>
+                </div>
+            </div>
         </div>
     )
 }
 
 export default HeroImage;
+
