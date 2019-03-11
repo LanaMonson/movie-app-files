9614078d5f7c14c3127539e24317b477d9f73421
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-03-11 15:02:07 -0600

    Update Home.js
    Load More Btn added

--- Home.js
+++ Home.js
@@ -94,6 +94,10 @@ class Home extends Component {
                                     />
                         }) }
                     </FourColGrid>
+                    { this.state.loading ? <Spinner /> : null }
+                    { (this.state.currentPage <= this.state.totalPages && !this.state.loading) ? 
+                        <LoadMoreBtn text="Load More" onClick={ this.loadMoreItems } />
+                        : null }
                 </div>
                 <Spinner />
                 <LoadMoreBtn />  
