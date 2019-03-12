6766ac429735e5f73222887b89b743a5d089c7d8
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-03-12 17:36:03 -0600

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import Header from '../elements/Header/Header';
 import Home from '../Home/Home';
 import Movie from '../Movie/Movie';
-import NorFound from '../elements/NotFound/NotFound';
+import NotFound from '../elements/NotFound/NotFound';
 
 const App = () => {
     return(
