3291830ce8151882e7e33a9530a063f84aa4cb55
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-03-12 15:46:21 -0600

    Update Movie.js
    imported components

--- Movie.js
+++ Movie.js
@@ -1,4 +1,12 @@
 import React, {Component} from 'react';
+import {API_URL, API_KEY} from '../../config';
+import Navigation from '../elements/Navigation/Navigation';
+import MovieInfo from '../elements/MovieInfo/MovieInfo';
+import MovieInfoBar from '../elements/MovieInfoBar/MovieInfo';
+import FourColGrid from '../elements/FourColGrid/FourColGrid';
+import Actor from '../elements/Actor/Actor';
+import Spinner from '../elements/Spinner/Spinner';
+import './Movie.css';
 
 class Movie extends Component{
     render() {
