c9d127e0ef788dd10ff72b023b06980b35579a9f
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-03-12 14:51:15 -0600

    Update Header.js
    Link added

--- Header.js
+++ Header.js
@@ -7,7 +7,9 @@ const Header = () => {
         //React.createElement('div', {className: 'react_div'}, 'This is a test' );
         <div className="rmdb-header">
             <div className="rmdb-header-content">
-                <img className="rmdb-logo" src="./images/reactMovie_logo.png" alt="rmdb=logo" />
+                <Link to="/">
+                    <img className="rmdb-logo" src="./images/reactMovie_logo.png" alt="rmdb=logo" />
+                </Link>
                 <img className="rmdb-tmdb-logo" src="./images/tmdb_logo.png" alt="tmdb=logo" />
             </div>
         </div>
