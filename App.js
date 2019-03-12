943498be852efabe066cd6fafd0f76fc1a96c4e7
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-03-12 14:26:12 -0600

    Update App.js
    Route added

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import Header from '../elements/Header/Header';
 import Home from '../Home/Home';
 
 const App = () => {
     return(
-        <div>
-            <Header />
-            <Home />
-        </div>
+        <BrowserRouter>
+            <React.Fragment>
+                <Header />
+                <Switch>
+                    <Route path="/" component={ Home } exact />
+                    <Route path="./:movieId" component={ Movie } exact />
+                    <Route component={ NotFound }/>
+                </Switch>
+            </React.Fragment>
+        </BrowserRouter>//line 13 :movieId - : colon - we specify here, its where we grab the movie ID
     )
 }
 
