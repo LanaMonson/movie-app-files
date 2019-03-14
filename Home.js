11dde7994fce2dd9f5992303b83f60fa946b4307
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-03-14 11:09:54 -0600

    Update Home.js

--- Home.js
+++ Home.js
@@ -19,9 +19,14 @@ class Home extends Component {
     }
 
     componentDidMount() {
-        this.setState({ loading: true });
-        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
-        this.fetchItems(endpoint);
+        if(localStorage.getItem('HomeState')) {
+            const state = JSON.parse(localStorage.getItem('HomeState'));
+            this.setState({ ...state});
+        } else {
+            this.setState({ loading: true });
+            const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+            this.fetchItems(endpoint);
+        }
     }
 
     searchItems = (searchTerm) => {
@@ -62,7 +67,11 @@ class Home extends Component {
                 heroImage: this.state.heroImage || result.results[0],//--> grab an image, from first movies in API fetch
                 loading: false, 
                 currentPage: result.page,//--> grabbing data from page (Inspect-> Console)
-                totalPages: result.total_pages,//
+                totalPages: result.total_pages//
+            }, () => {
+                if (this.state.searchTerm === "") {
+                localStorage.setItem('Home State', JSON.stringify(this.state));
+                } 
             })
         })
     } 
